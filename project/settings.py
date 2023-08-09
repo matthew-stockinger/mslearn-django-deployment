1f01f307e8f40be22604f091bc06536b3ed346d1
Matthew Stockinger <matt@matthewstockinger.com>
2023-08-09 09:39:00 -0700

    collect static

--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,9 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 ####### If running on Azure, add/override settings for production #########
-import os
+
 if 'WEBSITE_HOSTNAME' in os.environ:
     from .azure import *
